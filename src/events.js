9f1b57667181e19b210ba0595e8760a9ff1c89a7
lena <oleshko.lena@gmail.com>
2020-04-08 12:38:40 +0200

    Use static constructor to add pre-selected books to thelibrary

--- src/events.js
+++ src/events.js
@@ -13,14 +13,15 @@ function eventController() {
 
     //We first create a library from the existing books we have in 'books.js' module.
     //To do so(see function below createLibrary()), we create a class instance of Library (located in 'library.js' module).
-    //We iterate through our array of books and create a book instance from each object/book of the array.
+    //We iterate through our array of books and create a book instance from each object/book of the array. We use our static constructor within Library class to do so.
     //We add each book instance that was created to our library with the help of 
     //the method of our Library class 'add'.
     //We then go to our 'render.js' module and display each of our books.
     //After that we select the delete button and add an event listener.
     //! We rely on the fact that indices of delete buttons, of the cards that hold our books and
     //and of our book objects in the library class instance match.
-    const lib = createLibrary(list);
+    const library = Library.from(list);
+    displayBooks();
 
     newBookButton.addEventListener('click', render.showForm);
 
@@ -31,20 +32,17 @@ function eventController() {
         listenToDelete(currentDel);
     });
 
-    function createLibrary(books) {
-        let library = new Library(); // a new instance if Library class
-        for (let i = 0; i < books.length; i++) { // looping through array of book objects
-            library.add(books[i]); // adding books to the library
+    function displayBooks() {
+        for (let i = 0; i < library.library.length; i++) { // looping through library array inside ob Library class instance
             render.display(library.library[i]); //displaying each book on the page
             let button = delButtons[i]; //selecting delete button for each book
             listenToDelete(button); //adds event listener to the a book's delete button
         }
-        return library;
     }
 
     function addBook() {
         const book = new Book(newTitle.value, newAuthor.value, newYear.value);
-        lib.add(book);
+        library.add(book);
         render.display(book);
     }
 
@@ -58,7 +56,7 @@ function eventController() {
     }
 
     function removeBook(i) {
-        lib.delete(lib.library[i]);
+        library.delete(library.library[i]);
         render.hide(i);
     }
 
--- src/library.js
+++ src/library.js
@@ -9,6 +9,13 @@ class Library {
         let i = this.library.indexOf(book);
         if (i !== -1) this.library.splice(i, 1);
     }
+    static from(books) {
+        let lib = new Library();
+        for (let i = 0; i < books.length; i++) {
+            lib.add(books[i]);
+        }
+        return lib;
+    } 
 }
 
 export {Library}
